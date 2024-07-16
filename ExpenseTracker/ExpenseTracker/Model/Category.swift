ade8c445a6b7a17571b4c3e09ad23cc832319d73
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-16 15:05:48 +0300

    add Category enum model

---
+++ ExpenseTracker/ExpenseTracker/Model/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  ExpenseTracker
+//
+//  Created by Oleksandr Isaiev on 16.07.2024.
+//
+
+import SwiftUI
+
+enum Category: String {
+    case income = "Income"
+    case expense = "Expense"
+}
