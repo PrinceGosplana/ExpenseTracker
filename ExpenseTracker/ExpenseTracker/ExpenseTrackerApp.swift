30c75ad1c689cf19476e1b13b513696d2c662fbb
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-16 09:48:00 +0300

    base init

---
+++ ExpenseTracker/ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Oleksandr Isaiev on 16.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
