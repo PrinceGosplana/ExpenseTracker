7160a2b35216edc4f96f1b1776f256a93cf58fe6
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-18 18:37:21 +0300

    add base ui views for tabs

---
+++ ExpenseTracker/ExpenseTracker/View/TabViews/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  ExpenseTracker
+//
+//  Created by Oleksandr Isaiev on 18.07.2024.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings()
+}
