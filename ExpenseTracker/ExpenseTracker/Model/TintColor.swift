908b8188843e4bce904a85d6d26598e6bd96490a
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-18 16:03:24 +0300

    add tintcolor struct

---
+++ ExpenseTracker/ExpenseTracker/Model/TintColor.swift
@@ -0,0 +1,15 @@
+//
+//  TintColor.swift
+//  ExpenseTracker
+//
+//  Created by Oleksandr Isaiev on 18.07.2024.
+//
+
+import SwiftUI
+
+struct TintColor: Identifiable {
+    let id: UUID = .init()
+    let color: String
+    let value: Color
+}
+
