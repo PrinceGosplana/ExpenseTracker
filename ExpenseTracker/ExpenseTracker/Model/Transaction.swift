8fe7d862ef3c4293a657084cd30f55d641269f12
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-19 07:20:08 +0300

    sample transactions for ui building

--- ExpenseTracker/ExpenseTracker/Model/Transaction.swift
+++ ExpenseTracker/ExpenseTracker/Model/Transaction.swift
@@ -30,3 +30,11 @@ struct Transaction: Identifiable {
     }
 
 }
+
+/// Sample transactions for ui building
+var sampleTransactions: [Transaction] = [
+    .init(title: "Magic Keyboard", remarks: "Apple Product", amount: 129, dateAdded: .now, category: .expense, tintColor: tints.randomElement()!),
+    .init(title: "Apple Musuc", remarks: "Subscription", amount: 10.99, dateAdded: .now, category: .expense, tintColor: tints.randomElement()!),
+    .init(title: "iCloud", remarks: "Subscription", amount: 0.99, dateAdded: .now, category: .expense, tintColor: tints.randomElement()!),
+    .init(title: "Payment", remarks: "Payment Received!", amount: 2000, dateAdded: .now, category: .income, tintColor: tints.randomElement()!),
+]
