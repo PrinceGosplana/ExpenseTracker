9fc21a663a2e3f7fd4b9f0a88286bd4ff10dd6b3
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-23 10:11:11 +0300

    use lockview

--- ExpenseTracker/ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ExpenseTracker/ContentView.swift
@@ -11,30 +11,35 @@ struct ContentView: View {
     ///Intro Visibility status
     @AppStorage("isFirstTime") private var isFirstTime: Bool = true
 
+    /// App lock properties
+    @AppStorage("isAppLockEnabled") private var isAppLockEnabled: Bool = false
+    @AppStorage("lockWhenAppGoesBackground") private var lockWhenAppGoesBackground: Bool = false
     /// Active Tab
     @State private var activeTab: Tab = .recents
 
     var body: some View {
-        TabView(selection: $activeTab) {
-            Recents()
-                .tag(Tab.recents)
-                .tabItem { Tab.recents.tabContent }
+        LockView(lockType: .biometric, lockPin: "", isEnabled: isAppLockEnabled) {
+            TabView(selection: $activeTab) {
+                Recents()
+                    .tag(Tab.recents)
+                    .tabItem { Tab.recents.tabContent }
 
-            Search()
-                .tag(Tab.search)
-                .tabItem { Tab.search.tabContent }
+                Search()
+                    .tag(Tab.search)
+                    .tabItem { Tab.search.tabContent }
 
-            Graphs()
-                .tag(Tab.charts)
-                .tabItem { Tab.charts.tabContent }
+                Graphs()
+                    .tag(Tab.charts)
+                    .tabItem { Tab.charts.tabContent }
 
-            Settings()
-                .tag(Tab.settings)
-                .tabItem { Tab.settings.tabContent }
-        }
-        .sheet(isPresented: $isFirstTime) {
-            IntroScreen()
-                .interactiveDismissDisabled()
+                Settings()
+                    .tag(Tab.settings)
+                    .tabItem { Tab.settings.tabContent }
+            }
+            .sheet(isPresented: $isFirstTime) {
+                IntroScreen()
+                    .interactiveDismissDisabled()
+            }
         }
     }
 }
