74f0fd1d37f7a9922f5ffc7d57e9b05c0a8390da
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-26 11:45:05 +0300

    show category on transactioncardview

--- ExpenseTracker/ExpenseTracker/View/TabViews/Search.swift
+++ ExpenseTracker/ExpenseTracker/View/TabViews/Search.swift
@@ -24,7 +24,7 @@ struct Search: View {
                             NavigationLink {
                                 TransactionView(editTransaction: transaction)
                             } label: {
-                                TransactionCardView(transaction: transaction)
+                                TransactionCardView(transaction: transaction, showsCategory: true)
                             }
                             .buttonStyle(.plain)
                         }
