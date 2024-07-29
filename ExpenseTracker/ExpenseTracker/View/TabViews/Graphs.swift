0b047cc942eaccec45a01c630d35c4fbe34d89ad
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-29 09:57:23 +0300

    group charts

--- ExpenseTracker/ExpenseTracker/View/TabViews/Graphs.swift
+++ ExpenseTracker/ExpenseTracker/View/TabViews/Graphs.swift
@@ -15,17 +15,19 @@ struct Graphs: View {
     @State private var chartGroups: [ChartGroup] = []
 
     var body: some View {
-        ScrollView(.vertical) {
-            LazyVStack(spacing: 10) {
-                /// Chart view
-                ChartView()
-                    .padding(10)
-                    .frame(height: 200)
-                    .background(.background, in: .rect(cornerRadius: 10))
+        NavigationStack {
+            ScrollView(.vertical) {
+                LazyVStack(spacing: 10) {
+                    /// Chart view
+                    ChartView()
+                        .padding(10)
+                        .frame(height: 200)
+                        .background(.background, in: .rect(cornerRadius: 10))
+                }
             }
-        }
-        .onAppear {
-            
+            .onAppear {
+                createChartGroup()
+            }
         }
     }
 
@@ -48,6 +50,50 @@ struct Graphs: View {
         .chartScrollableAxes(.horizontal)
         /// Foreground colors
         .chartForegroundStyleScale(range: [Color.green.gradient, Color.red.gradient])
+    }
+
+    private func createChartGroup() {
+        Task.detached(priority: .high) {
+            let calendar = Calendar.current
+
+            let groupByDate = Dictionary(grouping: transactions) { transaction in
+                let components = calendar.dateComponents([.month, .year], from: transaction.dateAdded)
+
+                return components
+            }
+
+            /// Sorting groups by date
+            let sortedGroups = groupByDate.sorted {
+                let date1 = calendar.date(from: $0.key) ?? .init()
+                let date2 = calendar.date(from: $1.key) ?? .init()
+
+                return calendar.compare(date1, to: date2, toGranularity: .day) == .orderedDescending
+            }
+
+            let chartGroups = sortedGroups.compactMap { dict -> ChartGroup in
+                let date = calendar.date(from: dict.key) ?? .init()
+                let income = dict.value.filter({ $0.category == Category.income.rawValue })
+                let expense = dict.value.filter({ $0.category == Category.expense.rawValue })
+
+                let incomeTotalValue = total(income, category: .income)
+                let expenseTotalValue = total(expense, category: .expense)
+
+                return .init(
+                    date: date,
+                    categories: [
+                        .init(totalValue: incomeTotalValue, category: .income),
+                        .init(totalValue: expenseTotalValue, category: .expense)
+                    ],
+                    totalIncome: incomeTotalValue,
+                    totalExpense: expenseTotalValue
+                )
+            }
+
+            /// UI must be updated on Main thread
+            await MainActor.run {
+                self.chartGroups = chartGroups
+            }
+        }
     }
 }
 
