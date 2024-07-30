3559d6e8987fadb1bc7c7b649b4a22d71b83f3f8
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-30 11:09:15 +0300

    add widget

---
+++ ExpenseTracker/StatsCardView/StatsCardViewBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StatsCardViewBundle.swift
+//  StatsCardView
+//
+//  Created by Oleksandr Isaiev on 30.07.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StatsCardViewBundle: WidgetBundle {
+    var body: some Widget {
+        StatsCardView()
+    }
+}
